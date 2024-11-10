c740cc88f68da3ca8e082a0924b25dc91a6ba9b4
Abdulsobur <91859818+Abdulsobur-tech@users.noreply.github.com>
2024-11-10 19:05:47 +0100

    remove all js files

--- src/components/basicTable.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var BasicTable = function (_a) {
-    var columns = _a.columns, data = _a.data, heading = _a.heading;
-    return ((0, jsx_runtime_1.jsxs)("div", { className: "overflow-x-auto", children: [heading, (0, jsx_runtime_1.jsxs)("table", { className: "min-w-full table-auto border-collapse", children: [(0, jsx_runtime_1.jsx)("thead", { children: (0, jsx_runtime_1.jsx)("tr", { children: columns.map(function (column) { return ((0, jsx_runtime_1.jsx)("th", { className: "px-4 py-2 text-left border-b bg-gray-100 text-[#667085] text-[12px]", children: column.title }, column.key)); }) }) }), (0, jsx_runtime_1.jsx)("tbody", { children: data.map(function (row, index) { return ((0, jsx_runtime_1.jsx)("tr", { className: "hover:bg-gray-50", children: columns.map(function (column) { return ((0, jsx_runtime_1.jsx)("td", { className: "px-4 py-5 border-b", children: row[column.key] }, column.key)); }) }, index)); }) })] })] }));
-};
-exports.default = BasicTable;
